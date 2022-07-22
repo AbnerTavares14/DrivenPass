6284ccd1e7cada9da5acc2b7c1bbd31838ea184a
Abner <abnerdasilvatavares@gmail.com>
2022-07-22 13:59:08 -0300

    tests with supertest

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,4 @@ app.use(express.json());
 app.use(router);
 app.use(handleErrorsMiddleware);
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+export default app;
