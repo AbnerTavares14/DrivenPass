60ee62163af34077afb6a06421160cda284484f6
pedro imbrech <elprogramadorpedro@gmail.com>
2022-08-01 17:02:54 -0300

    rev

--- src/schemas/userSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-import { User } from "@prisma/client";
-
-const userSchema = Joi.object<User>({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(10).required()
-});
-
-export default userSchema;
